82231bb0adfba7937331d982894e634ff63cb344
YatinGarg1103 <72562645+YatinGarg1103@users.noreply.github.com>
2021-07-08 20:47:52 +0530

    Add files via upload

---
+++ screens/MyDonationScreen.js
@@ -0,0 +1,25 @@
+import React,{Component} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TouchableOpacity,
+  } from 'react-native';
+import db from '../config';
+import firebase from 'firebase';
+export default class MyDonationScreen extends Component {render(){
+return(
+    <View>
+        <Text>
+            welcome to my donation screen
+        </Text>
+    </View>
+)
+    
+
+
+
+
+}}
